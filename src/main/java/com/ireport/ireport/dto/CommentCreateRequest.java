00b7bc32f53f06efb724fa2d2ad11eafebc06539
annomao <omaoannkerubo@gmail.com>
2022-09-29 09:44:39 +0300

    Add dto files for comment and posts

--- src/main/java/com/ireport/ireport/dto/CommentCreateRequest.java
+++ src/main/java/com/ireport/ireport/dto/CommentCreateRequest.java
@@ -1,4 +1,16 @@
 package com.ireport.ireport.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class CommentCreateRequest {
+    @NotBlank(message = "Comment field can not be left blank")
+    private String commentBody;
+
+    public String getCommentBody() {
+        return commentBody;
+    }
+
+    public void setCommentBody(String commentBody) {
+        this.commentBody = commentBody;
+    }
 }
--- src/main/java/com/ireport/ireport/dto/PostCreateRequest.java
+++ src/main/java/com/ireport/ireport/dto/PostCreateRequest.java
@@ -1,4 +1,26 @@
 package com.ireport.ireport.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class PostCreateRequest {
+    @NotBlank(message = "Title can not be left blank")
+    private String title;
+    @NotBlank(message = "Post body can not be blank")
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
